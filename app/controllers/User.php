9c0616994ed6a5ef4d3b0cc1b2c31412278d38fb
violeta-919 <viowilles195@gmail.com>
2021-11-03 11:59:48 -0700

    view

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -1,12 +1,20 @@
 <?php
 
-class User {
+class User extends Controller{
   public function index() {
-    echo 'User/index';
+    $data["judul"] = "User";
+        $this->view("templates/header", $data);
+        $this->view("user/index");
+        $this->view("templates/footer");
   }
-  public function profile($nama = 'Linux', $pekerjaan = 'Devs') {
-    echo "Salam kenal saya $nama, saya seorang $pekerjaan";
-  }
+  public function profile($nama = "Linux", $pekerjaan = "Devs") {
+    $data["judul"] = "User";
+    $data["nama"] = $nama;
+    $data["pekerjaan"] = $pekerjaan;
+    $this->view("templates/header", $data);
+    $this->view("user/profile", $data);
+    $this->view("templates/footer");
+ }
 }
 
 ?>
---
+++ app/views/user/profile.php
@@ -0,0 +1,15 @@
+<html lang=”en”>
+
+ <head>
+   <meta charset=”UTF-8">
+   <meta name=”viewport” content=”width=device-width, initial-scale=1.0">
+   <meta http-equiv=”X-UA-Compatible” content=”ie=edge”>
+   <title>Halaman <?= $data['judul']; ?></title>
+ </head>
+
+ <body>
+   <h1>Halaman User</h1>
+   <p>Halo, nama saya <?= $data['nama']; ?>, saya seorang <?= $data['pekerjaan'];?></p>
+ </body>
+
+</html>
